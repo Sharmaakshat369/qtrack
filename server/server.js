efb824b52ce52d459b2c8d8faae743c1160fee81
Sharmaakshat369 <sharmaakshat3679@gmail.com>
2025-05-22 18:31:35 +0530

    routes added to server.js

--- server/server.js
+++ server/server.js
@@ -17,5 +17,9 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 // Routes will be added here
 
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/queue', require('./routes/queue'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
